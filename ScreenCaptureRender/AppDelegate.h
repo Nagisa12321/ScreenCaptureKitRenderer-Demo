3d6cbeaacbe6fcffeeae82d1dc329bec8ce1ea45
timjtchen <timjtchen@tencent.com>
2022-11-19 16:52:04 +0800

    Initial Commit

---
+++ ScreenCaptureRender/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ScreenCaptureRender
+//
+//  Created by jt on 2022/11/19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ScreenCaptureRender/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ScreenCaptureRender
+//
+//  Created by jt on 2022/11/19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+  }
+  return NSApplicationMain(argc, argv);
+}
